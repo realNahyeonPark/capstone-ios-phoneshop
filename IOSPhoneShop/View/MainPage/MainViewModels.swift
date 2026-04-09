aaeb2b6a777c44fcd54e1bc40788767eb5b53296
Nahyeon Park <lime2168@gmail.com>
2026-04-09 17:51:50 +0900

    쿠폰 이름 추가 검색화면 뒤로가기 버튼 활성화 찜하기 상품과 상세페이지 상품 연결 메인화면 정렬버튼 추가 UI수정 오류 수정

---
+++ IOSPhoneShop/View/MainPage/MainViewModels.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewModels.swift
+//  IOSPhoneShop
+//
+//  Created by nh on 4/9/26.
+//
+
+import SwiftUI
+
+struct MainViewModels: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainViewModels()
+}
